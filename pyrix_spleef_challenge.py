06b8c375f7e615a1fd40b318645dc621727ecf77
xanadian9001 <47192284+xanadian9001@users.noreply.github.com>
2019-02-14 17:15:22 -0500

    Update pyrix_spleef_challenge.py

--- pyrix_spleef_challenge.py
+++ pyrix_spleef_challenge.py
@@ -10,12 +10,6 @@
 # 4.  Repair *all* the holes (where there is minecraft:air) to "reset" the board.  The magma stays.
 # Note that the locations of events 1-3 are random.  You could easily change it so that the "warning blocks"
 # are placed UNDER some random player, but I didn't do that here.
-#
-# Forgot to mention:  the "repair all the holes" portion of the code may freeze your game for a short bit, or 
-# for longer than that.  :-/  If I can figure out a better way of filling the holes without having to scan every 
-# single block, then I'll update this script.  Wish I could just copy a schematic from a repository somewhere...
-# Alternatively, this thing can be reprogrammed to use a *square* board.  Then you could just use 
-# executeCommand("execute @s ~ ~ ~ fill etc etc etc)...
 
 import math
 import time
@@ -39,24 +33,19 @@ x = 100
 z = 100
 x_t = 100
 z_t = 100
-id = 0
+id = 600 # if you're combining this with the Wall-O-Fire, you better not have this at 0 or there will be a conflict
 event_thingie = {} # a dictionary of lists for id, x, and z (I hope)
 rebuild_complete = False
 
-"""
-class my_Clock:
-	def __init__(self, id, event, x, z):
-		self.id = id
-		self.event = event
-		self.x = x
-		self.z = z
-""" 
+def init(event):
+	event.npc.timers.clear()
 
 def timer(event):
 	
-	if event.id == 0:
-		result = "Timer ID is " + str(id) + " with coordinates x = " + str(x) + ", z = " + str(z) + " at time " + str(time.clock())
-		event.npc.say(result)
+	if event.id == 0 or event.id == 1: # keep the event.id == 1 if you're using rotating_fire_beam.py
+		#result = "Timer ID is " + str(id) + " with coordinates x = " + str(x) + ", z = " + str(z) + " at time " + str(time.clock())
+		#event.npc.say(result)
+		dummy = True
 	#elif event.id == id: # sometimes *this* triggers....
 		#x_t = event_thingie[id][0]
 		#z_t = event_thingie[id][1]
@@ -95,8 +84,8 @@ def tick(event):
 		a = randint(0,359) # at what angle from center
 		# now for some ...*gulp*... trigonometry.  what is new X and new Y?
 		radians = math.radians(a)
-		x = int(math.cos(a) * r) + x_c
-		z = int(math.sin(a) * r) + z_c
+		x = int(math.cos(radians) * r) + x_c
+		z = int(math.sin(radians) * r) + z_c
 		location = "x = " + str(x) + ", z = " + str(z)
 		if d_10 == 0 or d_10 == 1: # magma blocks
 			command = "execute @s " + str(x_c) + " " + str(y) + " " + str(z_c) + " fill " + str(x - 1) + " " + str(y) + " " + str(z - 1) + " " + str(x + 1) + " " + str(y) + " " + str(z + 1 ) + " minecraft:magma"
@@ -110,7 +99,7 @@ def tick(event):
 			event.npc.executeCommand(command)
 			command = "execute @s "  + str(x_c) + " " + str(y) + " " + str(z_c) + " playsound minecraft:weather.rain master @a "+ str(x) + " " + str(y + 0.75) + " " + str(z) +" 1 0.5"
 			event.npc.executeCommand(command)
-			id = int(time.clock()) # it's actually the "time", but I'm using it as a unique identifier for this particular object
+			id = int(time.clock())+600 # it's actually the "time", but I'm using it as a unique identifier for this particular object
 			event.npc.timers.forceStart(id,100,False)
 			# flip a coin ... testing to see if I can have multiple delayed events w/o it being a pain in the ass to code
 			coin = randint(0,1)
@@ -122,18 +111,18 @@ def tick(event):
 		else:
 			dummy = True
 	elif event.npc.world.getClosestEntity(event.npc.getPos(), 70, 1) != None: #someone is getting closer...or maybe moving out of the 50m range?...
-		# note...this causes significant lag for several seconds.  *shrug*  it *is* a command that is essentially activated 10k times...
-		# maybe someday I'll figure out a more efficient way of doing this
-		#event.npc.timers.clear()
-		#event_thingie = {} # garbage collection #### nope, this causes the NPC to leave the "warning blocks" behind. :-/
+		# this routine is for cleanup.  It's a *bit* laggy.  Just FYI.
 		# do a nested for loop that replaces all the holes with something solid
 		if rebuild_complete == False:
-			for x_this in range(-50,50):
-				for z_this in range(-50,50):
-					h = math.sqrt(x_this ** 2 + z_this ** 2)
+			for this_x in range(-50,50):
+				for this_z in range(-50,50):
+					h = math.sqrt(this_x ** 2 + this_z ** 2)
 					if h < 50:
-						command = "execute @s ~ ~ ~ setblock " + str(int(x_this + x_c)) + " " + str(y) + " " + str(int(z_this + z_c)) + " minecraft:obsidian 1 keep"
-						event.npc.executeCommand(command)
+						event.npc.getWorld().setBlock(this_x+x_c, y, this_z+x_c, "minecraft:obsidian", 0)
+						#command = "execute @s ~ ~ ~ setblock " + str(int(x_this + x_c)) + " " + str(y) + " " + str(int(z_this + z_c)) + " minecraft:obsidian 1 keep"
+						#event.npc.executeCommand(command)
 			rebuild_complete = True
+		#report = "execute @s ~ ~ ~ tell xanadian Out of range."
+		#event.npc.executeCommand(report)
 	else:
 		dummy = True
